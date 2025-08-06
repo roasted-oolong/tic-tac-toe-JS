b02e8f1ce6b0f2ee8965633c934e30278d6aebbf
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-08-06 09:59:23 -0500

    Create Gameboard, Cell, and GameController objects

--- js-file.js
+++ js-file.js
@@ -4,20 +4,144 @@ p.style.cssText = "color: blue";
 document.body.appendChild(p);
 
 /*
-Players need to be stored in objects
-Object should control the flow of the game itself
+** The Gameboard represents the state of the board
+** Each equare holds a Cell (defined later)
+** and we expose a dropToken method to be able to add Cells to squares
+*/
 
-The main goal is to have little global code as possible
-Try tucking as much as you can inside factories
-If you only need a single instance of something (e.g. gamboard, displayController),
-then wrap the factory inside an IIFE (module pattern) so it cannot be reused to create additional instances
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
 
-Think carefully about WHERE each bit of logic should reside.
-Each little piece of functionalty should be able to fit in the game, player, or gameboard onjects
-Take care to put them in "logcal" places. BRAINSTORM!!!
+  // Create a 2d array that will represent the state of the game board
+  // For this 2d array, row 0 will represent the top row and
+  // column 0 will represent the left-most column.
+  // This nested-loop technique is a simple and common way to create a 2d array.
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
 
-Step 1! Brainstorm
-Step 2! Get a working game in the console
-Step 3! Create object that will handle display/DOM logic
-Step 4! Write functions that allow players to add marks to specific spot on board
-Step 5!Clean up internface to allow players to put their inames, include start/restart game, and add display element that shows a results upon game end!!!
+  // This will be the method of getting the entire board that our
+  // UI will eventually need to render it.
+  const getBoard = () => board;
+
+  // In order to drop a token, we need to find what the lowest point of the
+  // selected column is, *then* change that cell's value to the player number
+  const dropToken = (column, player) => {
+    // Our board's outermost array represents the row,
+    // so we need to loop through the rows, starting at row 0,
+    // find all the rows that don't have a token, then take the
+    // last one, which will represent the bottom-most empty cell
+    const availableCells = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
+
+    // If no cells make it through the filter, 
+    // the move is invalid. Stop execution.
+    if (!availableCells.length) return;
+
+    // Otherwise, I have a valid cell, the last one in the filtered array
+    const lowestRow = availableCells.length - 1;
+    board[lowestRow][column].addToken(player);
+  };
+
+  // This method will be used to print our board to the console.
+  // It is helpful to see what the board looks like after each turn as we play,
+  // but we won't need it after we build our UI
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithCellValues);
+  };
+
+  // Here, we provide an interface for the rest of our
+  // application to interact with the board
+  return { getBoard, dropToken, printBoard };
+}
+
+/*
+** A Cell represents one "square" on the board and can have one of
+** 0: no token is in the square,
+** 1: Player One's token,
+** 2: Player 2's token
+*/
+
+function Cell() {
+  let value = 0;
+
+  // Accept a player's token to change the value of the cell
+  const addToken = (player) => {
+    value = player;
+  };
+
+  // How we will retrieve the current value of this cell through closure
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue
+  };
+}
+
+/* 
+** The GameController will be responsible for controlling the 
+** flow and state of the game's turns, as well as whether
+** anybody has won the game
+*/
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = Gameboard();
+
+  const players = [
+    {
+      name: playerOneName,
+      token: 1
+    },
+    {
+      name: playerTwoName,
+      token: 2
+    }
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (column) => {
+    // Drop a token for the current player
+    console.log(
+      `Dropping ${getActivePlayer().name}'s token into column ${column}...`
+    );
+    board.dropToken(column, getActivePlayer().token);
+
+    /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+
+    // Switch player turn
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  // Initial play game message
+  printNewRound();
+
+  // For the console version, we will only use playRound, but we will need
+  // getActivePlayer for the UI version, so I'm revealing it now
+  return {
+    playRound,
+    getActivePlayer
+  };
+}
+
+const game = GameController();
